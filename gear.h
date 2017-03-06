ddcec73ddd1abf2a693e4ce5ca296db83831676a
ArtLeQuint <a.delrue@gmx.fr>
2017-03-06 14:31:35 +0100

    Update gear.h

--- gear.h
+++ gear.h
@@ -6,6 +6,8 @@
 #define PANNE 2
 #define EN_OUVERTURE 3
 #define EN_FERMETURE 4
+#define ON true
+#define OFF false
 
 class Gear // classe abstraite
 {
@@ -16,6 +18,8 @@ public:
     virtual bool getAlarm();
     virtual int getState();
     virtual void switchState(int newState);
+    virtual void setAlarm(bool a);
+    virtual void sendSignal();
 
 private:
     int state;
