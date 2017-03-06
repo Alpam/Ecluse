e1b65f5bce4490975fd434daada5920966a9984e
ArtLeQuint <a.delrue@gmx.fr>
2017-03-06 13:34:03 +0100

    gear

--- gear.cpp
+++ gear.cpp
@@ -2,6 +2,23 @@
 
 Gear::Gear()
 {
+    state = 0;
+    alarm = false;
+}
+
+Gear::~Gear()
+{
 
 }
 
+void Gear::switchState(int newState) {
+   state = newState;
+}
+
+bool Gear::getAlarm() {
+   return alarm;
+}
+
+int Gear::getState() {
+   return state;
+}
--- gear.h
+++ gear.h
@@ -2,10 +2,20 @@
 #define GEAR_H
 
 
-class Gear
+class Gear // classe abstraite
 {
 public:
     Gear();
+    virtual ~Gear();
+    virtual void switchLightColor() = 0;
+    virtual bool getAlarm();
+    virtual int getState();
+    virtual void switchState(int newState);
+
+private:
+    int state;
+    bool alarm;
 };
+
 
 #endif // GEAR_H
